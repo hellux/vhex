2ae364a282ac9dd56dadcb4e2bcc2caeff767dd6
Noah Hellman <noah.hellman@protonmail.com>
2019-06-29 17:27:34 +0200

    write scroll percentage to status line

--- src/VHex/StatusLine.hs
+++ src/VHex/StatusLine.hs
@@ -3,15 +3,20 @@ module VHex.StatusLine (viewStatusLine) where
 import VHex.Types
 import VHex.Attributes (attrStatusLine)
 
-import Brick.Types (Widget)
-import Brick.Widgets.Core (withAttr, str)
+import Brick.Types (Widget, Padding(Max))
+import Brick.Widgets.Core (withAttr, str, padRight, hLimitPercent, hBox)
+
+-- TODO more accurate percentage using dimensions of editor
+scrollPercentage :: Int -> Int -> String
+scrollPercentage 0 _ = "Top"
+scrollPercentage scroll len = show p ++ "%" where
+    p :: Int
+    p = round $ (*100) $ (fromIntegral scroll :: Double) /
+                         (fromIntegral len :: Double)
 
 viewStatusLine :: Model -> Widget Name
-viewStatusLine m = withAttr attrStatusLine $ str $
-    filePath m ++ ": "
-    ++ show (cursorPos m) ++ ", "
-    ++ show (bufLen m) ++ "  "
-    ++ case mode m of
-        NormalMode _ -> ""
-        InputMode im input nb ->
-            "Input " ++ show im ++ " " ++ show input ++ " " ++ show nb
+viewStatusLine m = withAttr attrStatusLine $ hBox
+    [ hLimitPercent 85 $ padRight Max $ str $ filePath m
+    , padRight Max $ str $ show (cursorPos m)
+    , str $ scrollPercentage (scrollPos m) (bufLen m)
+    ]
